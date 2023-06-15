05277c6502be7a1d280f5681c7e445aa01ca21a0
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-06-14 20:28:54 -0400

    moving script

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let price = 400000;
+let amount = document.querySelector("#selectedAmount");
+let selectedColor = document.querySelector("#selectedColor");
+
+let button = document.querySelector("#calculateTotal")
+button.addEventListener("click", () => {
+document.querySelector("#total").innerHTML= amount.value * price;
+document.querySelector("#amount").innerHTML= amount.value;
+document.querySelector("#color").style.backgroundColor = selectedColor.value;
+})
